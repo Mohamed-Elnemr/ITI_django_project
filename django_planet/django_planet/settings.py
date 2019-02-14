286f3ac2b0f77ed9405792029c8f224899de567d
ziad <ziadmabdelhamed@gmail.com>
2019-02-14 17:59:04 +0200

    templates & STATICs

--- django_planet/django_planet/settings.py
+++ django_planet/django_planet/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'django_planet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,4 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'Statics/')
+
+LOGIN_REDIRECT_URL = 'login_success'
+
+
