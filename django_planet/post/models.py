3498acff26bd42b040553b4aee5c44b20b531dcc
ziad <ziadmabdelhamed@gmail.com>
2019-02-15 13:59:38 +0200

    update article model

--- django_planet/post/models.py
+++ django_planet/post/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from website.models import City
 
 # Create your models here.
 
 class Article(models.Model):
     article_id = models.PositiveIntegerField(primary_key=True)
-    article_type = models.CharField(max_length = 200 ,null=False)
-    article_type_id = models.IntegerField(null=False)
     article_content = models.CharField(max_length=250,null=False)
     article_rank = models.IntegerField()
+    city = models.ForeignKey(City)
     user = models.ForeignKey(User)
 
 
